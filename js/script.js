9d7e7d11448fbe95b8b097051c9b927bf83721bc
torone89 <torone89>
2022-06-22 15:12:19 +0200

    Start Java

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+// VERIFICA JS COLLEGATO
+
+console.log('JS OK');
+
+/* 
+
+1 Creare una variabile per contenere Nome del passeggero
+2 Creare una variabile per contenere kilometri e chiedere all'utente il numero dei kilometri che vuole percorrere 
+3 Calcolo il totale del viaggio senza sconto
+4 Stampo il totale del viaggio senza sconto e inserisco html
+5 Calcolo Sconti del 20 % e del 60%
+6 Quando devo applicare lo sconto?
+7 Stampo gli sconti sul doc e inserisco un nuovo div
+8 Valdazione
+
+*/
+
+const nomePasseggero = document.getElementById('nome')
+console.log(nomePasseggero)
+
